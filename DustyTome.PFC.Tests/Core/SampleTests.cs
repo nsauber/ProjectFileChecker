717421923cede0f516bf0e46ee48c173bfdbf25e
Nathan Sauber <nsauber@gmail.com>
2011-10-30 15:26:17 -0400

    Adding initial unit test project with placeholder test.

---
+++ DustyTome.PFC.Tests/Core/SampleTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace DustyTome.PFC.Tests.Core
+{
+    [TestFixture]
+    public class SampleTests
+    {
+        [Test]
+        public void SampleTest()
+        {
+            Assert.Pass();
+        }
+    }
+}
